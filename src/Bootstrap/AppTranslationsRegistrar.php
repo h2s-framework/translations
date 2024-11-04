fc4fbe960deadbd6287add5b834583c261d5a986
Siarko <labroqu@gmail.com>
2024-11-04 14:13:43 +0100

    Mark as module, update plugin for correct implementation

--- src/Bootstrap/AppTranslationsRegistrar.php
+++ src/Bootstrap/AppTranslationsRegistrar.php
@@ -3,13 +3,29 @@
 declare(strict_types=1);
 
 namespace Siarko\Translations\Bootstrap;
+use Siarko\ActionRouting\HttpApplication;
+use Siarko\Plugins\Config\Attribute\PluginMethod;
+use Siarko\Translations\Translations;
+
 class AppTranslationsRegistrar
 {
 
+    /**
+     * Used only for loading Translations Singleton
+     * @param Translations $translations
+     */
     public function __construct(
-        private \Siarko\Translations\Translations $translations
+        private readonly \Siarko\Translations\Translations $translations
     )
     {
     }
+
+    /**
+     * Method is here just to instantiate the class
+     * @param HttpApplication $subject
+     * @return void
+     */
+    #[PluginMethod]
+    public function beforeStart(HttpApplication $subject): void {}
 
 }
